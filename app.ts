b039137f40b8f8e68550d35e4bf104e1475e7c09
maikCyphlock <maikolaguilar656@gmail.com>
2023-12-12 11:55:23 -0400

    ✨ [ADD] swagger, Auth0 and dotenv to protect routes

--- app.ts
+++ app.ts
@@ -1,19 +1,62 @@
 import express from 'express'
 import helmet from 'helmet'
 import { UsersRoute } from './api/userRoutes'
+import { auth, requiresAuth } from 'express-openid-connect'
+import swaggerJsdoc from 'swagger-jsdoc'
+import swaggerUi from 'swagger-ui-express'
+import dotenv from 'dotenv'
+dotenv.config()
+const config = {
+  authRequired: process.env.authRequired ?? false,
+  auth0Logout: process.env.authLogout ?? true,
+  secret: process.env.secret ?? '',
+  baseURL: process.env.baseUrl ?? 'http://localhost:3000',
+  clientID: process.env.clientID ?? '',
+  issuerBaseURL: process.env.issuerBaseURL ?? ''
+}
 
 const app = express()
-
+const PORT = process.env.PORT ?? 3000
 app.use(express.json())
-// Routes
-app.use('/users', UsersRoute)
 
-app.get('/', (req, res) => {
-  res.send('Hello World')
-})
+const options = {
+  failOnErrors: true,
+  definition: {
+    openapi: '3.1.0',
+    info: {
+      title: 'My Express API',
+      version: '1.0.0',
+      description: 'A simple Express API'
+    },
+    servers: [
+      {
+        url: 'http://localhost:3000'
+      }
+    ]
+  },
+  apis: ['./api/*.ts']
+}
 
+const specs = swaggerJsdoc(options)
+
+app.use(
+  '/api-docs',
+  swaggerUi.serve,
+  swaggerUi.setup(specs)
+)
+app.use(
+  // @ts-expect-error
+  auth(config)
+)
 app.use(helmet())
 
-app.listen(3000, () => {
-  console.log('Server is running on port 3000')
+// Routes
+app.use('/users', requiresAuth(), UsersRoute)
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port http://localhost:${PORT}`)
 })
